9a8b8c8ec0afe85913600af8e2cedfdd49289e43
Cristóbal Valverde Albo <1251881+valverdealbo@users.noreply.github.com>
2022-05-08 10:44:19 +0200

    feat: add retry after to 429 and 503 errors

--- src/service-unavailable-error.ts
+++ src/service-unavailable-error.ts
@@ -1,7 +1,10 @@
 import { HttpError } from './http-error';
 
 export class ServiceUnavailableError extends HttpError {
-  constructor(message = 'The server is down and is not accepting requests.') {
+  public retryAfter: number;
+
+  constructor(message = 'The server is down and is not accepting requests.', retryAfter = 60) {
     super(503, 'ServiceUnavailableError', message);
+    this.retryAfter = retryAfter;
   }
 }
--- src/too-many-requests-error.ts
+++ src/too-many-requests-error.ts
@@ -1,7 +1,10 @@
 import { HttpError } from './http-error';
 
 export class TooManyRequestsError extends HttpError {
-  constructor(message = 'The user has sent too many requests in a given amount of time.') {
+  public retryAfter: number;
+
+  constructor(message = 'The user has sent too many requests in a given amount of time.', retryAfter = 60) {
     super(429, 'TooManyRequestsError', message);
+    this.retryAfter = retryAfter;
   }
 }
